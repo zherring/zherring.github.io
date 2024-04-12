084ea6883a65daf974f181ef89f6dc879e1d01be
Zach Herring <2172549+zherring@users.noreply.github.com>
2024-04-12 15:52:21 -0600

    setting up tailwind
    why? idk

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './build/css/output.css',
+    './build/js/bundle.js',
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
